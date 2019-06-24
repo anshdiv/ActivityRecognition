ff86b16ae844408152a759f636b619d2545364d7
Anshul Gour <anshul.gour@originitsolutions.com>
2019-06-24 16:39:16 +0530

    Intial Commit

---
+++ app/src/main/java/com/origin/activityrecognition/DetectedActivitiesIntentService.kt
@@ -0,0 +1,43 @@
+package com.origin.activityrecognition
+
+import android.app.IntentService
+import android.content.Intent
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.android.gms.location.ActivityRecognitionResult
+import com.google.android.gms.location.DetectedActivity
+
+/**
+ * @author Anshul Gour on 24,June,2019
+   Description : -
+ */
+class DetectedActivitiesIntentService : IntentService(TAG) {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
+        val result = ActivityRecognitionResult.extractResult(intent)
+
+        // Get the list of the probable activities associated with the current state of the
+        // device. Each activity is associated with a confidence level, which is an int between
+        // 0 and 100.
+        val detectedActivities = result.probableActivities as ArrayList<*>
+
+        for (activity in detectedActivities) {
+            broadcastActivity(activity as DetectedActivity)
+        }
+    }
+
+    private fun broadcastActivity(activity: DetectedActivity) {
+        val intent = Intent(MainActivity.BROADCAST_DETECTED_ACTIVITY)
+        intent.putExtra("type", activity.type)
+        intent.putExtra("confidence", activity.confidence)
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+    }
+
+    companion object {
+
+        protected val TAG = DetectedActivitiesIntentService::class.java.simpleName
+    }
+}// Use the TAG to name the worker thread.
